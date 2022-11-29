fc2e8f1833c954d4b996eedad22e17a52b6ec0d5
BIL-LAB5PC04\Ogrenci <123@123.COM>
2022-11-29 16:15:58 +0300

    no message

---
+++ U2_GOLDSORU2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_GOLDSORU2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < 1000; i++)
+                
+            {
+                Random rstgsayi = new Random();
+                pictureBox1.Left = pictureBox1.Left + rstgsayi.Next(1, 20);
+                    
+                pictureBox2.Left = pictureBox2.Left+rstgsayi.Next (1,20);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ odev2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c, enb, enk;
+            a = Int16.Parse(textBox1.Text);
+            b= Int16.Parse(textBox2.Text);
+            c= Int16.Parse(textBox3.Text);
+            enb = 0;
+            enk = 0;
+            if ((a > b) && (a > c)) ;
+            {
+                enb = a;
+            }
+            if((c>b) && (c>a));
+            {
+                enb = c;
+            }
+            if ((b > b) && (b > c)) ;
+            {
+                enb = b;
+            }
+            if ((a < b) && (a < c)) ;
+            {
+                enk = a;
+            }
+            if ((c < b) && (c < a)) ;
+            {
+                enk = c;
+            }
+            if ((c > b) && (b > a)) ;
+            {
+                enk = b;
+            }
+            MessageBox.Show(enk,"");
+        }
+
+    }
+}
