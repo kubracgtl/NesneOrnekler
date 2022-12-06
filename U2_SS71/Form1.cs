6bdebe4429ffe50e5cf8a5e1dcc8cb8785db3771
BIL-LAB5PC04\Ogrenci <123@123.COM>
2022-12-06 16:16:12 +0300

    no message

---
+++ U2_SS71/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_SS71
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte yuzde;
+            try
+            {
+                yuzde = Convert.ToByte(textBox1.Text);
+                progressBar1.Value = yuzde;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ U2_Uyg32/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_Uyg32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi, sayinin_karesi;
+            try
+            {
+                sayi = Convert.ToInt16(textBox1.Text);
+                sayinin_karesi = sayi * sayi;
+                MessageBox.Show(sayinin_karesi.ToString());
+
+            }
+            catch
+            {
+                MessageBox.Show("hatalı giriş yaptınız");
+            }
+
+            finally
+            {
+            }
+
+        }
+    }
+}
+
