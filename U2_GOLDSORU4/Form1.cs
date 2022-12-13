0077effe1d26d7f633326156ca5deae68fb81d94
BIL-LAB5PC04\Ogrenci <123@123.COM>
2022-12-13 15:19:23 +0300

    no message

---
+++ U2_GOLDSORU4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_GOLDSORU4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int deger, deger2;
+                deger = Convert.ToInt16(textBox1.Text);
+                deger2 = Convert.ToInt16(textBox2.Text);
+                for (int i = deger; i < deger2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        listBox2.Items.Add(i);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(i);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("lütfen sayı giriniz");
+            }
+        }
+    }
+}
---
+++ U2_goldsoru5/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2_goldsoru5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int deger, deger2;
+                deger = Convert.ToInt16(textBox1.Text);
+                deger2 = Convert.ToInt16(textBox2.Text);
+                for (int i = deger; i < deger2; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        listBox3.Items.Add(i);
+                    }
+                    else if (i%3==0)
+                    {
+                        listBox2.Items.Add(i);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(i);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("lütfen sayı giriniz");
+            }
+        }
+    }
+}
+
+        
