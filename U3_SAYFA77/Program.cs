0ddb1a5ae967060cfcf13e3194aff9f011100f15
BIL-LAB5PC04\Ogrenci <123@123.COM>
2022-12-27 16:13:52 +0300

    no message

---
+++ U3_SAYFA77/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace U3_SAYFA77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(8, 10);
+             Console.WriteLine("DİKDÖRTGENİN ALANI ")
+
+        }
+    }
+}
---
+++ U3_SS77/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire daire = new Daire(4);
+            Console.WriteLine("DAİRENİN alanı=" + daire.AlanHesapla());
+            Console.WriteLine("DAİRENİN cevresi=" + daire.CevreHesapla());
+            Console.ReadLine();
+
+                
+
+
+
+        }
+
+    }
+
+
+    class Daire
+    {
+        private double r;
+        public Daire(double r)
+        {
+            this.r = r;
+        }
+
+
+        public double AlanHesapla()
+        {
+            return 4* Math.PI * r;
+
+        }
+        public double CevreHesapla()
+        {
+            return (2 * Math.PI * r);
+        }
+    }
+}
