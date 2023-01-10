bff919392be6eba0a98f7ee9b9e4def9b4b9d3e8
BIL-LAB5PC04\Ogrenci <123@123.COM>
2023-01-10 10:57:46 +0300

    no message

---
+++ ODEVV7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEVV7
+{
+    class TrueFalse
+    {
+        public bool dogruyanlis(int sayi1)
+        {            
+            if (sayi1 % 2 == 0)
+                return false;
+            else
+                return true;
+           
+        }
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            TrueFalse tr = new TrueFalse();
+
+
+            Console.WriteLine("Sayı: {0}", tr.dogruyanlis(11));
+
+            Console.ReadLine();
+
+        }
+
+    }   
+}
---
+++ odev7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev7
+{
+     class Truefalse
+    {
+        /ry>
+        [STAThread]
+        static void Main()
+        {
+            double tf;
+            truefalse tf = new truefalse();
+            int a = 10;
+            int b = 5;
+            
+            Console.WriteLine("büyüksayi{0}", sonuc);
+            Console.ReadLine();
+        }
+    }
+}
