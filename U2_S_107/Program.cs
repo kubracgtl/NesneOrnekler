973ecdf51bebc23fc5c561770ff88e8f4607c069
BIL-LAB5PC04\Ogrenci <123@123.COM>
2023-01-17 10:53:48 +0300

    no message

---
+++ U2_S_107/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2_S_107
+{
+
+    interface IHayvan
+    {
+        void SesCikar();
+    }
+    interface IBeslen
+    {
+        void Beslen();
+    }
+    public class Kedi : IHayvan, IBeslen
+    {
+
+        public void SesCikar()
+        {
+            Console.WriteLine("kedi:miyavv");
+
+        }
+        public void Beslen()
+        {
+            Console.WriteLine("kedi süt içti");
+
+        }
+    }
+    public class Kopek: 
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
