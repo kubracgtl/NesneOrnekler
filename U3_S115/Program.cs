7d7804b62b86269f62d84f83ff079de3c7bc3252
kubracaglitalasligil@gmail.com <kubracaglitalasligil@gmail.com>
2023-02-21 13:31:38 +0300

    no message

---
+++ U3_S115/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S115
+{
+    enum Seviyeler
+    {
+      cok_dusuk,
+      dusuk,
+      orta,
+      yuksek,
+      col_yuksek,
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Seviyeler.cok_dusuk);
+            Console.WriteLine((int)Seviyeler.cok_dusuk);
+            Console.ReadLine();
+        }
+    }
+}
