d05c6ef0ba5d9d2aa9325ac2d1d2c0ef20731008
BIL-LAB1PC07\Ogrenci <123@123.com>
2023-03-14 14:37:21 +0300

    no message

---
+++ U5_UYG5/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace U5_UYG5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TXTBİLGİ = new System.Windows.Forms.TextBox();
+            this.ncorba = new System.Windows.Forms.NumericUpDown();
+            this.ntatlı = new System.Windows.Forms.NumericUpDown();
+            this.nanayemek = new System.Windows.Forms.NumericUpDown();
+            this.nsalataa = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ncorba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ntatlı)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nanayemek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsalataa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 264);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 238);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "MÜŞTERİ";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.nsalataa);
+            this.tabPage2.Controls.Add(this.nanayemek);
+            this.tabPage2.Controls.Add(this.ntatlı);
+            this.tabPage2.Controls.Add(this.ncorba);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 238);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "SİPARİŞ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.TXTBİLGİ);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(192, 238);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "HESAP";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // TXTBİLGİ
+            // 
+            this.TXTBİLGİ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TXTBİLGİ.Location = new System.Drawing.Point(0, 0);
+            this.TXTBİLGİ.Name = "TXTBİLGİ";
+            this.TXTBİLGİ.Size = new System.Drawing.Size(192, 20);
+            this.TXTBİLGİ.TabIndex = 0;
+            // 
+            // ncorba
+            // 
+            this.ncorba.Location = new System.Drawing.Point(69, 75);
+            this.ncorba.Name = "ncorba";
+            this.ncorba.Size = new System.Drawing.Size(86, 20);
+            this.ncorba.TabIndex = 7;
+            // 
+            // ntatlı
+            // 
+            this.ntatlı.Location = new System.Drawing.Point(66, 158);
+            this.ntatlı.Name = "ntatlı";
+            this.ntatlı.Size = new System.Drawing.Size(120, 20);
+            this.ntatlı.TabIndex = 8;
+            // 
+            // nanayemek
+            // 
+            this.nanayemek.Location = new System.Drawing.Point(69, 129);
+            this.nanayemek.Name = "nanayemek";
+            this.nanayemek.Size = new System.Drawing.Size(107, 20);
+            this.nanayemek.TabIndex = 9;
+            // 
+            // nsalataa
+            // 
+            this.nsalataa.Location = new System.Drawing.Point(69, 101);
+            this.nsalataa.Name = "nsalataa";
+            this.nsalataa.Size = new System.Drawing.Size(76, 20);
+            this.nsalataa.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ncorba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ntatlı)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nanayemek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nsalataa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox TXTBİLGİ;
+        private System.Windows.Forms.NumericUpDown nsalataa;
+        private System.Windows.Forms.NumericUpDown nanayemek;
+        private System.Windows.Forms.NumericUpDown ntatlı;
+        private System.Windows.Forms.NumericUpDown ncorba;
+    }
+}
+
---
+++ U5_UYG5/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_UYG5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex==2)
+            {
+                TXTBİLGİ.Text = "";
+                TXTBİLGİ.Text+=textBox1.Text+"\r\n";
+                TXTBİLGİ.Text+=textBox2.Text+"\r\n";
+                TXTBİLGİ.Text+=textBox3.Text+"\r\n";
+                decimal hesap = 0;
+                if (ncorba.Value > 0)
+                {
+                    hesap += ncorba.Value * 50;
+                    TXTBİLGİ.Text += string.Format("Çorba {0:C}", ncorba.Value * 50) + "\r\n";
+                }
+                else if (nsalataa.Value > 0)
+                {
+                    hesap += nsalataa.Value * 20;
+                    TXTBİLGİ.Text += string.Format("salata{0:c}", nsalataa.Value * 20);
+                }
+                else if (nanayemek.Value >0)
+                {
+                    hesap += nanayemek.Value * 100;
+                    TXTBİLGİ.Text += string.Format("anayemek{0:c}", nanayemek.Value * 100);
+
+                }
+                else
+                {
+                    hesap += ntatlı.Value * 40;
+                    TXTBİLGİ.Text += string.Format("tatlı{0:c}", ntatlı.Value * 40);
+                }
+                TXTBİLGİ.Text += "-----------------";
+                TXTBİLGİ.Text += string.Format("Toplam {0:C}", hesap);
+            }
+        }
+    }
+}
