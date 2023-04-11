796b2aa5583a8e648b5137f808eb83bbf99317bb
BIL-LAB1PC07\Ogrenci <123@123.com>
2023-04-11 11:11:46 +0300

    no message

---
+++ U5_UYG15/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_UYG15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.SetToolTip(textBox1, "adınızı giriniz");
+            tt.SetToolTip(textBox2, "soyadınızı giriniz");
+            tt.SetToolTip(textBox3, "dogum tarihinizi giriniz");
+            tt.SetToolTip(textBox4, "adresinizi giriniz");
+
+            tt.SetToolTip(button1, "Kaydetmek için tıklayınız.");
+        }
+    }
+}
---
+++ U5_UYG16/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_UYG16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+
+            {
+                MessageBox.Show("kullanıcı adınız giriniz", "uyarı");
+            }
+            else
+            {
+                MessageBox.Show("şifreyi giriniz", "uyarı");
+            }
+        }
+    }
+}
